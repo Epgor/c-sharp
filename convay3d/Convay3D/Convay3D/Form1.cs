85581ba6e7dfaa34b18b6fd749d9aac4126ed4e5
Epgor <61190141+Epgor@users.noreply.github.com>
2021-09-27 20:28:32 +0200

    Add files via upload

---
+++ convay3d/Convay3D/Convay3D/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using OpenGL;
+
+namespace Convay3D
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OpenGL.Platform.Window.CreateWindow("OpenGL", 1280, 720);
+        }
+    }
+}
